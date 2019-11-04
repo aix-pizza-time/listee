d1cbcae1ff656f0147369086fd4badcb51f06eca
Alexander Bartolomey <occloxium@gmail.com>
2019-11-04 16:08:26 +0100

    initialized client

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'IBM Plex Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+}
+</style>
+
+<script>
+import Home from '@/views/Home'
+
+export default {
+  components: {
+    Home
+  }
+}
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>PizzaBot 🍕🍕</h1>
+    <List></List>
+  </div>
+</template>
+
+<script>
+import List from '@/components/List'
+
+export default {
+  name: 'Home',
+  components: {
+    List
+  }
+}
+</script>
+
+<style lang="sass">
+h1
+  font-size: 3em
+</style>
